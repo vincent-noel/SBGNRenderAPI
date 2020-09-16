86c1e637773cb6f5a761810e62959c074bc9fdc6
Vincent Noël <contact@vincent-noel.fr>
2020-09-16 22:15:29 +0200

    First draft of github page

--- api/RendererAPI.wsgi
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-# Adding this directory to python path
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Importing our application
-from RendererAPI import api as application
-
--- script.py
+++
@@ -1,7 +0,0 @@
-from sbgnrender import renderSBGN
-
-renderSBGN("Reaction_Species.xml", "machin.png", verbose=True)
-renderSBGN("Reaction_Species.xml", "machin.svg", format="svg")
-renderSBGN("Reaction_Species.xml", "machin2.png", bg="#f00")
-# renderSBGN("Reaction_Species.xml", "machin3.png", bg="#f00", layout=True) // Layout is not working
-# renderSBGN("Reaction_Species.xml", "machin2.svg", format="svg", bg="#f00") // Here something is wrong, all the image is red
