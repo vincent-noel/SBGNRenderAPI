3d08cd4f8dca2ee3ec7c5ba686fc4fe478ba51e3
Vincent Noël <contact@vincent-noel.fr>
2020-09-17 18:19:18 +0200

    Increase timeout in apache configuration

--- scripts/apache_config.sh
+++ scripts/apache_config.sh
@@ -3,7 +3,8 @@ SBGNREST_CFG=/etc/apache2/sites-available/sbgn_rest.conf
 echo $SBGNREST_CFG
 LINE=`cat $CFG | grep -n "</VirtualHost>" | cut -d":" -f1`
 head -n $((LINE-1)) $CFG > $SBGNREST_CFG
-echo "\tWSGIDaemonProcess RendererAPI user=www-data group=www-data threads=5" >> $SBGNREST_CFG
+echo "\tTimeout 7200" >> $SBGNREST_CFG
+echo "\tWSGIDaemonProcess RendererAPI user=www-data group=www-data threads=5 request-timeout=7200" >> $SBGNREST_CFG
 echo "\tWSGIScriptAlias / /var/sbgn-rest-renderer/api/RendererAPI.wsgi" >> $SBGNREST_CFG
 echo "\t<Directory /var/sbgn-rest-renderer>" >> $SBGNREST_CFG
 echo "\t\tWSGIProcessGroup RendererAPI" >> $SBGNREST_CFG
